baf2c55b790cb6b8d39cc4cdf3b04cdd33808807
IndieProof <37383570+IndieProof@users.noreply.github.com>
2019-01-31 20:09:30 -0800

    v. 1.0.1.1 Required fork name changes
    XDNA to ArCh/ain

--- src/archaind.cpp
+++ src/archaind.cpp
@@ -3,6 +3,7 @@
 // Copyright (c) 2014-2015 The Dash developers
 // Copyright (c) 2015-2017 The PIVX developers
 // Copyright (c) 2017-2018 The XDNA Core developers
+// Copyright (c) 2018-2019 The ArCh/ain developers @INDIEproof,LLC.
 // Distributed under the MIT/X11 software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
@@ -25,8 +26,9 @@
  *
  * \section intro_sec Introduction
  *
- * This is the developer documentation of the reference client for an experimental new digital currency called XDNA (http://www.xdna.io),
- * which enables instant payments to anyone, anywhere in the world. XDNA uses peer-to-peer technology to operate
+ * This is the developer documentation of the reference client for ArCh/ain's cryptocurrency called ACASH.
+ * This fork is based on the experimental new digital currency called XDNA (http://www.xdna.io),
+ * which enables instant payments to anyone, anywhere in the world. ArCh/ain uses peer-to-peer technology to operate
  * with no central authority: managing transactions and issuing money are carried out collectively by the network.
  *
  * The software is a community-driven open source project, released under the MIT license.
@@ -65,18 +67,18 @@ bool AppInit(int argc, char* argv[])
     //
     // Parameters
     //
-    // If Qt is used, parameters/xdna.conf are parsed in qt/xdna.cpp's main()
+    // If Qt is used, parameters/archain.conf are parsed in qt/archain.cpp's main()
     ParseParameters(argc, argv);
 
     // Process help and version before taking care about datadir
     if (mapArgs.count("-?") || mapArgs.count("-help") || mapArgs.count("-version")) {
-        std::string strUsage = _("XDNA Core Daemon") + " " + _("version") + " " + FormatFullVersion() + "\n";
+        std::string strUsage = _("ArCh/ain Daemon") + " " + _("version") + " " + FormatFullVersion() + "\n";
 
         if (mapArgs.count("-version")) {
             strUsage += LicenseInfo();
         } else {
             strUsage += "\n" + _("Usage:") + "\n" +
-                        "  xdnad [options]                     " + _("Start XDNA Core Daemon") + "\n";
+                        "  archaind [options]                     " + _("Start ArCh/ain Daemon") + "\n";
 
             strUsage += "\n" + HelpMessage(HMM_BITCOIND);
         }
@@ -112,17 +114,17 @@ bool AppInit(int argc, char* argv[])
         // Command-line RPC
         bool fCommandLine = false;
         for (int i = 1; i < argc; i++)
-            if (!IsSwitchChar(argv[i][0]) && !boost::algorithm::istarts_with(argv[i], "xdna:"))
+            if (!IsSwitchChar(argv[i][0]) && !boost::algorithm::istarts_with(argv[i], "archain:"))
                 fCommandLine = true;
 
         if (fCommandLine) {
-            fprintf(stderr, "Error: There is no RPC client functionality in xdnad anymore. Use the xdna-cli utility instead.\n");
+            fprintf(stderr, "Error: There is no RPC client functionality in archaind anymore. Use the acash-cli utility instead.\n");
             exit(1);
         }
 #ifndef WIN32
         fDaemon = GetBoolArg("-daemon", false);
         if (fDaemon) {
-            fprintf(stdout, "XDNA server starting\n");
+            fprintf(stdout, "ACASH server starting\n");
 
             // Daemonize
             pid_t pid = fork();
@@ -175,7 +177,7 @@ int main(int argc, char* argv[])
 {
     SetupEnvironment();
 
-    // Connect xdnad signal handlers
+    // Connect archaind signal handlers
     noui_connect();
 
     return (AppInit(argc, argv) ? 0 : 1);
